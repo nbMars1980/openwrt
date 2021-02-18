3023458a2013bbc8aae90eddcf9a13598e1c9ac4
marsgrace <47632876+marsgrace@users.noreply.github.com>
2021-02-18 16:44:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,6 +12,19 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.2/g' package/base-files/files/bin/config_generate
+# Edit Compiled name 
 sed -i  "/DISTRIB_REVISION='R/{s|\(.\+\)'\(.\+\)'\(.\+\)|\1'\2 Compiled by Mars'\3|;;}" package/lean/default-settings/files/zzz-default-settings
 #sed -i "s|DISTRIB_REVISION='R\(.\+\)\.\(\w\+\)|& Compiled by Mars|" package/lean/default-settings/files/zzz-default-settings
 #sed -i "s/DISTRIB_DESCRIPTION='OpenWrt/&_Mars/" package/lean/default-settings/files/zzz-default-settings
+
+#edit dhcp config
+sed -i "/exit 0/i\sed -i \"s/option start '100'/option ignore '1'/\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\sed -i \"/option limit /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\sed -i \"/option leasetime /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\sed -i \"/option ra /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\sed -i \"/option dhcpv6 /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\sed -i \"/option ndp /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\sed -i \"/option ra_management /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
+sed -i "/exit 0/i\sed -i \"/option ra_management /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
+#添加空行
+sed -i "/exit 0/{x;p;x}" package/lean/default-settings/files/zzz-default-settings
