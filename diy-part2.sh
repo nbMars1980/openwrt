3d3f3d37f9594f9f36aab3fdd344b5f4ab67cc76
marsgrace <47632876+marsgrace@users.noreply.github.com>
2021-02-19 10:43:40 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -29,6 +29,5 @@ sed -i "/exit 0/i\sed -i \"/option ra /d\" /etc/config/dhcp" package/lean/defaul
 sed -i "/exit 0/i\sed -i \"/option dhcpv6 /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
 sed -i "/exit 0/i\sed -i \"/option ndp /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
 sed -i "/exit 0/i\sed -i \"/option ra_management /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
-sed -i "/exit 0/i\sed -i \"/option ra_management /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
 #添加空行
 sed -i "/exit 0/{x;p;x}" package/lean/default-settings/files/zzz-default-settings
