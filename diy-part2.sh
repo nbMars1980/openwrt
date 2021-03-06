dda7f946587340f1e9e161cb4f129abb85667beb
marsgrace <47632876+marsgrace@users.noreply.github.com>
2021-03-06 14:09:22 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -24,17 +24,13 @@ find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-bypass/Makefile" | xargs -
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-vssr/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-redir/shadowsocksr-libev-alt/g' {}
 find package/*/ feeds/*/ -maxdepth 2 -path "*luci-app-vssr/Makefile" | xargs -i sed -i 's/shadowsocksr-libev-ssr-server/shadowsocksr-libev-server/g' {}
 
-#尝试修正libnetwork编译错误
-#find package/*/ feeds/*/ -maxdepth 3 -path "*libnetwork/Makefile"| xargs -i sed -i 's|(GO_PKG)/cmd/proxy|(GO_PKG)/tree/master/cmd/proxy|g' {}
-#find package/*/ feeds/*/ -maxdepth 3 -path "*libnetwork/Makefile"| xargs -i sed -i 's|(GO_PKG)/cmd/dnet|(GO_PKG)/tree/master/cmd/dnet|g' {}
-
 #edit dhcp config
-sed -i "/exit 0/i\sed -i \"s/option start '100'/option ignore '1'/\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
-sed -i "/exit 0/i\sed -i \"/option limit /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
-sed -i "/exit 0/i\sed -i \"/option leasetime /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
-sed -i "/exit 0/i\sed -i \"/option ra /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
-sed -i "/exit 0/i\sed -i \"/option dhcpv6 /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
-sed -i "/exit 0/i\sed -i \"/option ndp /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
-sed -i "/exit 0/i\sed -i \"/option ra_management /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
+#sed -i "/exit 0/i\sed -i \"s/option start '100'/option ignore '1'/\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
+#sed -i "/exit 0/i\sed -i \"/option limit /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
+#sed -i "/exit 0/i\sed -i \"/option leasetime /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
+#sed -i "/exit 0/i\sed -i \"/option ra /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
+#sed -i "/exit 0/i\sed -i \"/option dhcpv6 /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
+#sed -i "/exit 0/i\sed -i \"/option ndp /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
+#sed -i "/exit 0/i\sed -i \"/option ra_management /d\" /etc/config/dhcp" package/lean/default-settings/files/zzz-default-settings
 #添加空行
-sed -i "/exit 0/{x;p;x}" package/lean/default-settings/files/zzz-default-settings
+#sed -i "/exit 0/{x;p;x}" package/lean/default-settings/files/zzz-default-settings
