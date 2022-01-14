e54a97e7249dde4d7aed31d2d5777141faa87526
marsgrace <47632876+marsgrace@users.noreply.github.com>
2022-01-14 22:48:30 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,13 +21,13 @@ sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 #Add bypass
-git clone https://github.com/garypang13/luci-app-bypass package/lean/luci-app-bypass
+#git clone https://github.com/garypang13/luci-app-bypass package/lean/luci-app-bypass
 
 #Add ttnode
-git clone https://github.com/jerrykuku/luci-app-ttnode package/lean/luci-app-ttnode
+#git clone https://github.com/jerrykuku/luci-app-ttnode package/lean/luci-app-ttnode
 
 #Add dockerman
 #git clone https://github.com/lisaac/luci-app-dockerman package/lean/luci-app-dockerman
 
 #Add tencentddns
-git clone https://github.com/pbx168/luci-app-tencentddns package/lean/luci-app-tencentddns
+#git clone https://github.com/pbx168/luci-app-tencentddns package/lean/luci-app-tencentddns
