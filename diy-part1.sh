83186b0e86cdb255b0d5c348f559769eb33dfdb5
marsgrace <47632876+marsgrace@users.noreply.github.com>
2022-01-23 22:14:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,7 +18,7 @@
 #git clone https://github.com/kenzok8/openwrt-packages.git feeds/kenzo
 #sed -i '$a src-git kenzo https://github.com/Mars-Grace/openwrt-packages' feeds.conf.default
 sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 
 #Add bypass
