42d88a2ee29e152f8ea007e02ba3482215869f8d
mars_grace <mars_grace@qq.com>
2023-05-09 12:09:34 +0800

    修改配置

--- diy-part3.sh
+++ diy-part3.sh
@@ -2,8 +2,8 @@
 # File name: diy-part3.sh
 # Description: Modify Config File,delete docker and use uhttpd
 
-Use_Uhttpd=false
-remove_Docker=true
+Use_Uhttpd=true
+remove_Docker=false
 
 if $Use_Uhttpd ; then
 # remove nginx and use uhttpd
