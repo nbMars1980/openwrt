23ade7f2d23c33a7436f5107e2593840959f86c0
nbMars1980 <108038855+nbMars1980@users.noreply.github.com>
2024-11-27 13:07:50 +0800

    Update diy-packages.sh

--- diy-packages.sh
+++ diy-packages.sh
@@ -1,3 +1,6 @@
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+
 ./scripts/feeds update -a 
 
 rm -rf feeds/luci/applications/luci-app-mosdns feeds/luci/applications/luci-app-smartdns feeds/luci/applications/luci-app-aliyundrive-webdav feeds/luci/applications/luci-app-passwall feeds/luci/applications/luci-app-passwall2
