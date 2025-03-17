1a9e9ebbcc76d8493f73239f92657e2d55c12e09
Mars <mars_grace@qq.com>
2025-03-17 22:33:32 +0800

    1

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,7 +11,7 @@
 #
 
 # Add a feed source
-sed -i 's|github.com/coolsnowwolf/luci.git;.*|github.com/coolsnowwolf/luci.git|g' feeds.conf.default
+#sed -i 's|github.com/coolsnowwolf/luci.git;.*|github.com/coolsnowwolf/luci.git|g' feeds.conf.default
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
