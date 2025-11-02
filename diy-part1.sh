a52eda051a5126a49eba3495fb8e9e8eb65d272f
Mars <mars_grace@qq.com>
2025-11-02 09:36:21 +0800

    Dead Code Elimination

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,32 +15,11 @@
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
-#Add tencentddns
-#git clone https://github.com/pbx168/luci-app-tencentddns package/lean/luci-app-tencentddns
-
 ./scripts/feeds update -a 
 
 rm -rf feeds/luci/applications/luci-app-mosdns feeds/luci/applications/luci-app-smartdns feeds/luci/applications/luci-app-aliyundrive-webdav feeds/luci/applications/luci-app-passwall feeds/luci/applications/luci-app-passwall2
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/multimedia/aliyundrive-webdav 
 rm -rf feeds/packages/utils/v2dat
-
-#使用kenzok8的golang新版本
-#rm -rf feeds/packages/lang/golang
-#git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-
-## dnsmasq用2.9版本   24-8-31修改
-#### 使用 Git 的稀疏检出功能
-git clone --no-checkout https://github.com/kenzok8/small-package.git
-cd small-package
-git sparse-checkout init
-git sparse-checkout set dnsmasq
-git checkout main
-#### Git 的稀疏检出完毕
-rm -rf ../package/network/services/dnsmasq
-cp -r dnsmasq ../package/network/services/   
-cd ..
-rm -rf small-package
-## dnsmasq用2.9版本代码结束
 
 ./scripts/feeds install -a 
--- diy-part2.sh
+++ diy-part2.sh
@@ -10,27 +10,8 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
-#部分插件强制使用使用kenzo的，已移至diy-packages.sh实现了
-#cp .config .config.bak
-#./scripts/feeds uninstall smartdns luci-app-smartdns aliyundrive-webdav luci-app-aliyundrive-webdav xray-core sing-box v2ray-core luci-app-passwall luci-app-passwall2 
-#./scripts/feeds install -fp kenzo smartdns luci-app-smartdns aliyundrive-webdav luci-app-aliyundrive-webdav
-#./scripts/feeds install -fp small xray-core sing-box v2ray-core luci-app-passwall luci-app-passwall2
-#mv -f .config.bak .config
-
-
 #tomato界面主题中，passwall和passwall2菜单active存在冲突解决
 sed -i 's/if (href.indexOf(nodeUrl) != -1)/if (href.substr(href.length-nodeUrl.length,nodeUrl.length) == nodeUrl)/g' feeds/kenzo/luci-theme-tomato/htdocs/luci-static/tomato/js/script.js
-#passwall和passwall2  qrcode.min.js冲突   24-8-31修改
-# ###rm -f feeds/small/luci-app-passwall2/htdocs/luci-static/resources/qrcode.min.js
-
-######上游已打补丁，xray相关的可以直接用golang1.21编译了
-#xray-core需要golang1.22才能编译    24-8-31修改
-##rm -rf feeds/packages/lang/golang
-##git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
-
-
-#禁用firewill 启用firewill4  24-8-31修改
-#sed -i 's/+firewall/+uci-firewall/g' feeds/luci/applications/luci-app-firewall/Makefile
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.0.250/g' package/base-files/files/bin/config_generate
@@ -44,33 +25,6 @@ displayver=
 
 # remove default root password
 sed -i '/root\(.*\)shadow/d' package/lean/default-settings/files/zzz-default-settings
-
-# modify ttnode and jd-dailybonus notify server url
-#sed -i "s|https://\(.*\)ftqq.com/|http://192.168.0.3:2443/|g" package/lean/luci-app-ttnode/root/usr/share/ttnode/ttnode.lua
-# feeds/kenzo/luci-app-ttnode/root/usr/share/ttnode/ttnode.lua 
-#sed -i "s|https://\(.*\)ftqq.com/|http://192.168.0.3:2443/|g" package/lean/luci-app-jd-dailybonus/root/usr/share/jd-dailybonus/newapp.sh
-# feeds/kenzo/luci-app-jd-dailybonus/root/usr/share/jd-dailybonus/newapp.sh  
-
-# Modify tencentddns menu index
-sed -i '/腾讯云设置/i\entry({"admin", "services", "tencentddns"},cbi("tencentddns"),_("TencentDDNS"),59)' package/lean/luci-app-tencentddns/luasrc/controller/tencentddns.lua
-sed -i "/腾讯云设置/,+1d" package/lean/luci-app-tencentddns/luasrc/controller/tencentddns.lua
-
-# Modify ttnode menu index
-#sed -i "s|_('甜糖星愿自动采集'), 0)\.dependent|_('甜糖星愿自动采集'), 100)\.dependent|g" package/lean/luci-app-ttnode/luasrc/controller/ttnode.lua
-# fix ttnode
-#sed -i 's|src="/ttnode/jquery.min.js|src="/luci-static/ttnode/jquery.min.js|' package/lean/luci-app-ttnode/luasrc/view/ttnode/login_form.htm
-#sed -i '/if.*(d.error == 0)/{n;s/settime()/countdown = 60\;\n\t\t\t\t\t&/g}' package/lean/luci-app-ttnode/luasrc/view/ttnode/login_form.htm
-#sed -i '/jq.cookie(.ltime., 0)/{n;s/countdown = 60/\/\/&/g}' package/lean/luci-app-ttnode/luasrc/view/ttnode/login_form.htm
-
-# Edit theme-mcat css and js
-#sed -i '/a\[data-title="Docker"\]:before/{p;N;N;d}'  feeds/kenzo/luci-theme-mcat/files/htdocs/css/style.css
-#sed -i '/a\[data-title="Docker"\]:before/a\ content: "\\e025";\n color: #66CC00!important;' feeds/kenzo/luci-theme-mcat/files/htdocs/css/style.css
-#sed -i "s/if (href.indexOf(nodeUrl) != -1) {/if (href.substr(href.length-nodeUrl.length,nodeUrl.length) == nodeUrl) {/g" feeds/kenzo/luci-theme-mcat/files/htdocs/js/script.js
-
-# Edit theme-tomato css
-#sed -i '/a\[data-title="Docker"\]:before/{p;N;N;d}'  feeds/kenzo/luci-theme-tomato/htdocs/luci-static/tomato/cascade.css 
-#sed -i '/a\[data-title="Docker"\]:before/a\ content: "\\e025";\n color: #66CC00!important;' feeds/kenzo/luci-theme-tomato/htdocs/luci-static/tomato/cascade.css 
-#sed -i "s/if (href.indexOf(nodeUrl) != -1) {/if (href.substr(href.length-nodeUrl.length,nodeUrl.length) == nodeUrl) {/g" feeds/kenzo/luci-theme-tomato/htdocs/luci-static/tomato/js/script.js
 
 # Modify default close dhcpv6
 #sed -i '/dhcp.lan.\(.*$MODE\)/ s/^/# /g' package/network/services/odhcpd/files/odhcpd.defaults
@@ -87,8 +41,7 @@ cp -f files/luci.locations feeds/packages/net/nginx/files-luci-support/
 sed -i '/exit\(.*0\)/d' package/lean/default-settings/files/zzz-default-settings
 cat files/ext-default-settings >> package/lean/default-settings/files/zzz-default-settings
 
-
-#  尝试修复 ERROR: package/feeds/packages/uwsgi failed to build.问题
+#  修复 ERROR: package/feeds/packages/uwsgi failed to build.问题
 if [ -d "feeds/packages/net/uwsgi" ]; then
   echo "Upgrading uwsgi from 2.0.20 (coolsnowwolf) to 2.0.30 (openwrt/packages)..."
   # Backup original
